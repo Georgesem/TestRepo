6b018d683852f048d43e0e2bfa2594ee04cf9223
Georgesem <48730005+Georgesem@users.noreply.github.com>
2021-11-23 18:05:22 +0200

    task2

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,20 +6,9 @@ namespace ConsoleApp
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("ВВедите строку");
-			string str = Console.ReadLine();
-			Console.WriteLine("ВВедите символ");
-			string simbol = Console.ReadLine();
-			
-			float counter_char = 0;
-			foreach ( char ch in str) {
-		
-				if (ch == Convert.ToChar(simbol)){
-					counter_char++;
-                }
-            }
-			float result = counter_char / str.Length * 100;
-			Console.WriteLine("Процент вхождения символа " + simbol + " в строку " + str + " - " + result);
+			string str = "qwe qwe qwe qwe qwe qwe qew qwe qwe qew qew qwe qwe qwe qwe qwe qe";
+			string[] words = str.Split(' ');
+			Console.WriteLine("колличество слов в стоке " + str + " - " + words.Length);
 		}
 	}
 }
